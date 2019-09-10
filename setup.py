d397a02cbf0932d3a6463dcc47dfc177c8e35033
Nils Ziehn <nziehn@googlemail.com>
2019-09-10 22:34:00 +0200

    make sure package can be uploaded to pypi

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from os import path
 import setuptools
 
 # read the contents of your README file
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+# this_directory = path.abspath(path.dirname(__file__))
+# with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+#     long_description = f.read()
 
 dependencies = [
     'numpy',
@@ -12,14 +12,14 @@ dependencies = [
 
 setuptools.setup(
     name='toolbox-geohash',
-    version='1.0.0',
+    version='0.1.0',
     description=(
         'Module to encode any coordinate on earth to an integer and back to coordinates.\n'
         'In comparison to typical geohashing approaches in this library you can configure the '
         'accuracy of the hashing be defining the desired size of the triangles that are used.'
     ),
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    # long_description=description,
+    # long_description_content_type='text/markdown',
     url='http://github.com/nziehn/toolbox-geohash',
     author='Nils Ziehn',
     author_email='nziehn@gmail.com',
